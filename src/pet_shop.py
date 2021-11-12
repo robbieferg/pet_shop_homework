b35dc021ecf8cb75bf9dc8a54fd0205b8be25801
Robbie Ferguson <robbiejferguson92@gmail.com>
2021-11-12 16:23:38 +0000

    5th commit

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -8,8 +8,9 @@ def get_total_cash(pet_shop):
     
 def add_or_remove_cash(pet_shop, num):
     pet_shop["admin"]["total_cash"] += num
-    return pet_shop["admin"]["total_cash"]
     
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
     
+def increase_pets_sold(pet_shop, num):
+    pet_shop["admin"]["pets_sold"] += num
