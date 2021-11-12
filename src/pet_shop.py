17d6031a14deb47962259d1623701715a9eba207
Robbie Ferguson <robbiejferguson92@gmail.com>
2021-11-12 17:15:06 +0000

    10th commit

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -30,3 +30,8 @@ def find_pet_by_name(pet_shop, pet_name):
         if pet["name"] == pet_name:
             return pet
     return None
+
+def remove_pet_by_name(pet_shop, pet_name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == pet_name:
+            pet_shop["pets"].remove(pet)
